4a23dc636ba9308185debc514cb60480980db081
Icon-guderian <78599235+Icon-guderian@users.noreply.github.com>
2021-02-06 17:17:07 +0100

    New line in Zm

--- OperacionesZm.java
+++ OperacionesZm.java
@@ -27,7 +27,7 @@ public class OperacionesZm {
                 if(c == 0) {
                     if(a < 0 & b < 0 | a > 0 & b > 0){
                         System.out.println("La ecuación se queda de la forma ax = b tal que: "+ a +"x = "+ b);
-               
+                        System.out.println();
                     } else if (a < 0 & b > 0 | a > 0 & b < 0) {
                         System.out.println("La ecuación se queda de la forma ax = -b tal que: "+ a +"x = "+ b); 
                         
